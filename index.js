ad027634b8df5fbbd11a418511202928034f5600
mohammademon393 <mdemon357ep@gmail.com>
2026-06-24 18:55:47 +0600

    Zaft-shift-server connect to mongoDB

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const express = require('express');
-const cors = require('cors');
+// Load environment variables
+require("dotenv").config();
+
+// DNS configuration (Fix MongoDB Atlas SRV lookup issue)
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+// Package imports
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// App initialization
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware
-require('dotenv').config();
-app.use(express.json());
+// Middleware
 app.use(cors());
+app.use(express.json());
 
+// MongoDB Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vknfgr8.mongodb.net/?appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// server running
 app.get('/', (req, res) => {
     res.send('zap shifting server running...');
 });
