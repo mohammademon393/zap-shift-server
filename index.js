28caa41869ef3452fad70782e3708053b0484988
mohammademon393 <mdemon357ep@gmail.com>
2026-06-24 18:11:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+require('dotenv').config();
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('zap shifting server running...');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
